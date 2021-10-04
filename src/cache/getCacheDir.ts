ad5c8e8577e4870f846dadb897c3baa4e9fb6650
Aaron Carlino <unclecheese@leftandmain.com>
2021-10-05 11:43:30 +1300

    New os tmpdir

--- src/cache/getCacheDir.ts
+++ src/cache/getCacheDir.ts
@@ -1,6 +1,8 @@
-import getProjectDir from "../utils/getProjectDir"
+//import getProjectDir from "../utils/getProjectDir"
 import path from "path"
+import os from "os"
 
-const getCacheDir = (): string => path.join(getProjectDir() ?? ``, `.ss-cache`)
+//const getCacheDir = (): string => path.join(getProjectDir() ?? ``, `.ss-cache`)
 
+const getCacheDir = (): string => path.join(os.tmpdir(), `ss-cache`)
 export default getCacheDir
